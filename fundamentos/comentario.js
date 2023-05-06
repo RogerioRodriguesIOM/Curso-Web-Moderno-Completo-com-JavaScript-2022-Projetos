171b8cdf75d63c9a7d8f1c7d967ad617a0a58429
Rogerio Rodrigues <rogerio.rodrigues.br@hotmail.com>
2023-05-06 20:51:34 +0000

    adding changes

---
+++ fundamentos/comentario.js
@@ -0,0 +1,14 @@
+
+//console.log eh usado para exibir algo na tela do computador.
+console.log('Texte bem legal #01');
+console.log('Texte bem legal #03'); // mais um exemplo de console.log.
+// console.log('Texte bem legal #02'); colocando as duas barras eu anulo a execucao da linha de codigo. 
+
+/* 
+Esse eh um comentario de varias linhas e... 
+sera EXECUTADO se usar 
+'barra e asteriscos' 
+no inicio e final.
+*/
+
+
---
+++ sentenca_2.js
@@ -0,0 +1,4 @@
+
+console.log('Texte bem legal #01');
+console.log('Texte bem legal #03');
+console.log('Texte bem legal #02');
