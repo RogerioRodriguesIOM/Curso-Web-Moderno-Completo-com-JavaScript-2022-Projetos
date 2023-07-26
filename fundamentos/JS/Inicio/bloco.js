7f00f7e66c174d522fd8894f757c41939cccfa73
ROGERIO RODRIGUES <131472641+RogerioRodriguesIOM@users.noreply.github.com>
2023-07-26 22:59:34 +0100

    Organizacao do programa = Blocos, Sentencas de codigos, etc

---
+++ fundamentos/JS/Inicio/bloco.js
@@ -0,0 +1,3 @@
+// A sentenca de codigos eh cada uma das linhas de codigos terminadas com ; 
+// O Boclo eh tudo que estiver entre chaves {}. E dentro dos blocos pode haver varioas sentencas de codigos, que terminam com ;
+//
