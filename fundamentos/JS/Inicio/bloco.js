293abb9105649f3aa115fadaf55be179a9f2cfbf
ROGERIO RODRIGUES <131472641+RogerioRodriguesIOM@users.noreply.github.com>
2023-07-26 23:04:23 +0100

    JS - Exemplo de blocos de codigos

--- fundamentos/JS/Inicio/bloco.js
+++ fundamentos/JS/Inicio/bloco.js
@@ -1,3 +1,17 @@
 // A sentenca de codigos eh cada uma das linhas de codigos terminadas com ; 
 // O Boclo eh tudo que estiver entre chaves {}. E dentro dos blocos pode haver varioas sentencas de codigos, que terminam com ;
-//
+// Eh possivel abrir varios blocos de codigos dentro de outro 
+//ex: 
+{
+  console.log("Passo #01");
+  console.log("Passo #02");
+  console.log("Passo #03");
+  {
+    console.log("Passo #04");
+    console.log("Passo #05")
+  }
+}
+{
+  console.log("Passo #06");
+  console.log("Passo #07")
+}
