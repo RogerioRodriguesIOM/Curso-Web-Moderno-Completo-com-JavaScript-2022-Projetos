95ebfca3d373f18462a2a1599fbfa9ce3a53e1ca
ROGERIO RODRIGUES <131472641+RogerioRodriguesIOM@users.noreply.github.com>
2023-07-28 00:11:29 +0100

    JS - definicao de Variaveis

---
+++ fundamentos/JS/Inicio/dados_02.js
@@ -0,0 +1,7 @@
+//* quando criamos variaveis, usamos LET ou VAR e o nome que identificara a variavel. eh importante criar bons nomes para essa identificacao. depois usamos o sinal de = e a atribuicao de valor ou identificacao da variavel. posteriormente poderemos alterar a idade no codogo. Veja o ex abaixo: *//  
+//* var idade = 4; *//
+
+
+
+
+
