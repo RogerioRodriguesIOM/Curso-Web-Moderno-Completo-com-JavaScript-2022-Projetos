4d067e3bf4e152e7c181cf33d2d588aeaa4485a2
ROGERIO RODRIGUES <131472641+RogerioRodriguesIOM@users.noreply.github.com>
2023-07-28 23:20:29 +0100

    JS - Typeof = Number, Boolean, String

--- fundamentos/JS/Inicio/dados_03.js
+++ fundamentos/JS/Inicio/dados_03.js
@@ -1,12 +1,13 @@
+// NUmber
 let idade = 31;
 console.log(typeof 31);
 console.log(typeof idade);
 
 let salario = 4578.32;
 console.log(typeof salario);
-
+// Boolean:
 let estaChovendo = falso // ou true
 console.log(typeof estaChovendo);
-
+// String
 console.log(typeof "Teste");
 console.log(typeof 'Teste');
