40f3c4b2965fb42241f4bf5843a4204344c3efaf
ROGERIO RODRIGUES <131472641+RogerioRodriguesIOM@users.noreply.github.com>
2023-07-28 23:02:05 +0100

    js dados - complemento

--- fundamentos/JS/Inicio/dados_02.js
+++ fundamentos/JS/Inicio/dados_02.js
@@ -1,7 +1,14 @@
 //* quando criamos variaveis, usamos LET ou VAR e o nome que identificara a variavel. eh importante criar bons nomes para essa identificacao. depois usamos o sinal de = e a atribuicao de valor ou identificacao da variavel. posteriormente poderemos alterar a idade no codogo. Veja o ex abaixo: *//  
 //* var idade = 4; *//
 
+let preco = 19.90;
+let desconto = 0.4;
 
+console.log(19.90 * 0.6);
+console.log(preco * 1(- desconto));
+
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
 
 
 
