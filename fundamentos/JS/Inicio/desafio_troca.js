97fbcd8bef460f83dae0e7d39a934786890228c0
ROGERIO RODRIGUES <131472641+RogerioRodriguesIOM@users.noreply.github.com>
2023-07-29 04:29:35 +0100

    JS - Answer desafio troca

---
+++ fundamentos/JS/Inicio/desafio_troca.js
@@ -0,0 +1,14 @@
+let a = 7
+let b = 94
+
+let a = 7;
+let b = 94;
+// Trocar os valores das variáveis usando uma variável auxiliar
+let temp = a;
+a = b;
+b = temp;
+console.log("Agora a = " + a + " e b = " + b + ".");
+
+// trocar os valores das variaveis, tornando a = 94 e b = 7.
+console.log(a)
+console.log(b)
