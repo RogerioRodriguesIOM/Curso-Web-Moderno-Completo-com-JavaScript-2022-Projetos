b8379614aefaffa90209dacc4535aced6590ac2f
ROGERIO RODRIGUES <131472641+RogerioRodriguesIOM@users.noreply.github.com>
2023-07-29 04:12:37 +0100

    JS - Answer desafio raio circunferencia

--- fundamentos/JS/Inicio/desafio_area.js
+++ fundamentos/JS/Inicio/desafio_area.js
@@ -3,5 +3,10 @@
 // Calcular o raio da circunferencia:
 // crair uma variavel para representar o PI, outra para representar a area (que eh o resultado da operacao) e colocar no console o resultado da area eh: (o valor em metros quadrados). Usar uma string para colocar metros quadrados no resultado. 
 
+// PI * raio * raio
+
 let raio = 10;
+const PI = Math.PI;
+let area = PI * raio * raio;
+console.log("A área é: " + area + " metros quadrados.");
 
