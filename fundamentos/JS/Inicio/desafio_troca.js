f898a2fab04810e5d5e363dc0e21d9bb06a0abce
ROGERIO RODRIGUES <131472641+RogerioRodriguesIOM@users.noreply.github.com>
2023-07-29 04:41:52 +0100

    JS - alternativa de resposta

--- fundamentos/JS/Inicio/desafio_troca.js
+++ fundamentos/JS/Inicio/desafio_troca.js
@@ -6,6 +6,9 @@ a = b;
 b = temp;
 console.log("Agora a = " + a + " e b = " + b + ".");
 
+// uma alternativa seria usar o codigo abaixo:
+// [a, b] = [b, a]
+
 // trocar os valores das variaveis, tornando a = 94 e b = 7.
 console.log(a)
 console.log(b)
